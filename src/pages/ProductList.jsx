49d884c5320465d4e30975022a51ef8f8a07d622
mttaskin <0mttaskin1@gmail.com>
2023-03-04 13:05:13 +0300

    v3

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -11,13 +11,20 @@ const ProductList = () => {
   const url=process.env.REACT_APP_API_URL
 
 const [products, setProducts] = useState([])
+const [loading, setLoading] = useState(true)
+const [errorState, setErrorState] = useState(false)
 
 const getProducts =async () =>{
+
+  setLoading(false)
    try{
     const {data}= await axios(url)
     setProducts(data)
+    setErrorState(false)
+
     } catch (error) {
       console.log(error)
+      setErrorState(true)
     }
   }
 
@@ -31,18 +38,22 @@ const getProducts =async () =>{
   return (
     <div className="container mt-3">
       <div className={"bg-light d-sm-block d-md-flex"}>
-        <p className="text-center text-danger w-100">Loading....</p>
-
+        {loading ? <p className="text-center text-danger w-100">Loading....</p>
+        : products.length>0 ?
         <>
           <article id="product-panel" className="col-md-5">
-            <ProductCard />
+            {products.map((item)=>{
+              return <ProductCard item={item}/>
+            })}
           </article>
           <article className="col-md-5 m-3">
             <CardTotal />
           </article>
         </>
+        : (!errorState && 
+        <p className="text-center text-danger w-100">No products data...</p>)}
 
-        <p className="text-center text-danger w-100">No products data...</p>
+        {errorState && (<p className="text-center text-danger w-100">Error...</p>)}
       </div>
     </div>
   );
