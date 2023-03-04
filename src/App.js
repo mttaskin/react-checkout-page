41e611c8920e9d937da1c23088d1f94e20e1b772
mttaskin <0mttaskin1@gmail.com>
2023-03-04 11:42:36 +0300

    react-checkout-page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from "./pages/Main";
+import NewProduct from "./pages/NewProduct";
+import ProductList from "./pages/ProductList";
+import UpdateProduct from "./pages/UpdateProduct";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-light app">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/NewProduct" element={<NewProduct />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/ProductList" element={<ProductList />} />
+          <Route path="/" element={<UpdateProduct />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
